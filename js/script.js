b14902024a34b7280c40b5f605b8096821d0b7df
konojunya <junya03dance@gmail.com>
2015-11-17 17:58:43 +0900

    テスト

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
 $(function(){
-  var s = skrollr.init();
-
-  $('.fa-angle-double-up').on("click",function(){
-    $('html,body').animate({
-      scrollTop: 0
-    },3000);
-  });
-
   var _ua = (function(u){
     return {
       Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1)
@@ -24,12 +16,39 @@ $(function(){
         || u.indexOf("blackberry") != -1
     }
   })(window.navigator.userAgent.toLowerCase());
-  if(_ua.Mobile || _ua.Tablet){
-    $('.pc').hide();
-    $('.mobile').show();
-    $('#top2 i').hide();
-  }else{
-    $('.pc').show();
-    $('.mobile').hide();
-  }
+  $('a[href^=#]').on('click',function(){
+    var header_height = $('.navi').height();
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - header_height + 1;
+    if(_ua.Mobile || _ua.Tablet){
+      $('.navi').fadeOut();
+      $('fa-remove').hide();
+      $('.fa-bars').fadeIn();
+    }
+    $("html, body").animate({
+      scrollTop:position
+    }, 700);
+    return false;
+  });
+
+  $('.hello').typed({
+    strings: ["Welcome to my page !","Hello, My World!"],
+    typeSpeed: 0
+  });
+
+  setTimeout(function(){
+    $('.topAni').fadeOut('slow');
+  },5*1000);
+
+  $('.fa-bars').on('click',function(){
+    $('.navi').fadeIn();
+    $(this).hide();
+    $('.fa-remove').fadeIn();
+  });
+  $('.fa-remove').on('click',function(){
+    $('.navi').fadeOut();
+    $(this).hide();
+    $('.fa-bars').fadeIn();
+  });
 });
